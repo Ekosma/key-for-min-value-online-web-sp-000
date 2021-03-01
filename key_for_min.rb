dbd34b0dd5d2da9536084c453820fb82f459a43d
Evelyn Kosma <ezranaja@gmail.com>
2021-03-01 01:02:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,11 @@
 
 def key_for_min_value(name_hash)
   smallest_value == 0
-  
+  smallest_key = nil
   name_hash.each do |key, value|
-    if value < 0 
-      
+    if value < 0 || value < smallest_key
+      smallest_value = value
+      smallest_key = key
+    end
+  end
 end
